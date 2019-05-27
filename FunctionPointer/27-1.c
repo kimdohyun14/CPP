37b722e5e8d67e6e8cb62462cd4f1b1e6dad56e8
kimdohyun <201411239@sangmyung.kr>
2019-05-27 22:41:24 +0900

    Function pointer

---
+++ FunctionPointer/27-1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void FuncOne(void)
+{
+	printf("FuncOne 함수가 호출되었다! \n");
+}
+
+void FuncTwo(void)
+{
+	printf("FuncTwo 함수가 호출되었다! \n");
+}
+
+int main()
+{
+	FuncOne();
+	FuncTwo();
+	printf("FuncOne의 주소 값 : %#x \n", FuncOne);
+	printf("FuncTwo의 주소 값 : %#x \n", FuncTwo);
+	return 0;
+}
