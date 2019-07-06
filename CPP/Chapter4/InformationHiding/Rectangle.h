1c637350faa66492a1df92990c91eca6a497c7a1
kimdohyun <201411239@sangmyung.kr>
2019-07-06 13:54:03 +0900

    2019-07-06

---
+++ CPP/Chapter4/InformationHiding/Rectangle.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Point.h"
+
+class Rectangle {
+private:
+	Point upLeft;
+	Point lowRight;
+
+public:
+	bool InitMembers(const Point &ul, const Point &lr);
+	void ShowRecInfo() const;
+};
