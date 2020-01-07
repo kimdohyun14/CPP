e44f839953dab7991d04f7c2956d24da2dcc3e9b
KimDoHyun <dohyun9565@gmail.com>
2020-01-08 00:56:44 +0900

    Add files via upload

---
+++ C++/Chapter11/4_ImproveInit.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+// 이니셜라이저가 성능 향상에 도움을 준다.
+class AAA
+{
+private:
+	int num;
+public:
+	AAA(int n = 0) : num(n)
+	{
+		cout << "AAA(int n = 0)" << endl;
+	}
+	AAA(const AAA& ref) : num(ref.num)
+	{
+		cout << "AAA(const AAA& ref)" << endl;
+	}
+	AAA& operator=(const AAA& ref)
+	{
+		num = ref.num;
+		cout << "AAA& operator=(const AAA& ref)" << endl;
+		return *this;
+	}
+};
+
+class BBB
+{
+private:
+	AAA mem;
+public:
+	BBB(const AAA& ref) : mem(ref) { }
+};
+
+class CCC
+{
+private:
+	AAA mem;
+public:
+	CCC(const AAA& ref)
+	{
+		mem = ref;
+	}
+};
+
+int main()
+{
+	AAA obj1(12);
+	cout << "*******************************" << endl;
+	BBB obj2(obj1);
+	cout << "*******************************" << endl;
+	CCC obj3(obj1);
+	return 0;
+}
