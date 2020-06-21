4b85e1c907cbf8f9a6993316fb1aee603a86ac75
Kim Do Hyun <dohyun9565@gmail.com>
2020-06-21 19:04:18 +0900

    사칙 연산자 오버로딩

---
+++ C++/ModoCode/ComplexNumber_ver1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+class Complex
+{
+private:
+	double real, img;
+
+public:
+	Complex(double real, double img) : real(real), img(img) {}
+	Complex(const Complex& c) { real = c.real, img = c.img; }
+
+	Complex operator+(const Complex& c);
+	Complex operator-(const Complex& c);
+	Complex operator*(const Complex& c);
+	Complex operator/(const Complex& c);
+
+	void println() { std::cout << "( " << real << " , " << img << " )" << std::endl; }
+};
+
+Complex Complex::operator+(const Complex& c)
+{
+	Complex temp(real + c.real, img + c.img);
+	return temp;
+}
+
+Complex Complex::operator-(const Complex& c)
+{
+	Complex temp(real - c.real, img - c.img);
+	return temp;
+}
+
+Complex Complex::operator*(const Complex& c)
+{
+	// a1a2 - b1b2 + i(a1b2 + a2b1)
+	Complex temp(real * c.real - img * c.img, real * c.img + img * c.real);
+	return temp;
+}
+
+Complex Complex::operator/(const Complex& c)
+{
+	// a1a2 + b1b2 + i(a2b1 - a1b2)
+	// ----------------------------
+	//         a2a2 + b2b2
+	Complex temp(
+		(real * c.real + img * c.img) / (c.real * c.real + c.img * c.img),
+		(img * c.real - real * c.img) / (c.real * c.real + c.img * c.img)
+	);
+	return temp;
+}
+
+int main()
+{
+	Complex a(1.0, 2.0);
+	Complex b(3.0, -2.0);
+
+	Complex c = a * b;
+
+	c.println();
+	return 0;
+}
