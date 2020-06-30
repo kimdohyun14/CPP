aa99ca2f54eb40948a7767d0e0db16746bcead59
Kim Do Hyun <dohyun9565@gmail.com>
2020-06-30 19:28:16 +0900

    virtual 소멸자가 필요한 이유

---
+++ C++/ModoCode/inheritance_ver7.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+// virtual 소멸자가 필요한 이유
+
+class Parent
+{
+public:
+	Parent() { std::cout << "Parent 생성자 호출" << std::endl; }
+	virtual ~Parent() { std::cout << "Parent 소멸자 호출" << std::endl; }
+};
+
+class Child : public Parent
+{
+public:
+	Child() : Parent() { std::cout << "Child 생성자 호출" << std::endl; }
+	~Child() { std::cout << "Child 소멸자 호출" << std::endl; }
+};
+
+int main()
+{
+	std::cout << "--- 평범한 Child 를 만들었을 때 ---" << std::endl;
+	{
+		Child c;
+	}
+
+	std::cout << "--- Parent 포인터로 Child 가리켰을 때 ---" << std::endl;
+	{
+		Parent* p = new Child();
+		delete p;
+	}
+	return 0;
+}
