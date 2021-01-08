3b540ea54c22505bc315b74792f4e24dea619239
KimDoHyun <dohyun9565@gmail.com>
2021-01-08 21:54:38 +0900

    branch syntax

---
+++ C++/CppPrimerPlus/Chapter06/cingolf.cpp
@@ -0,0 +1,34 @@
+// cingolf.cpp -- 수가 아닌 입력은 무시한다
+#include <iostream>
+const int Max = 5;
+int main()
+{
+	using namespace std;
+
+	int golf[Max];
+	cout << "골프 점수를 입력하십시오.\n";
+	cout << "총 " << Max << " 라운드 점수를 입력해야 합니다.\n";
+	int i;
+	for (i = 0; i < Max; i++)
+	{
+		cout << "round #" << i + 1 << ": ";
+		while (!(cin >> golf[i]))
+		{
+			cin.clear(); // 입력을 초기화한다
+			while (cin.get() != '\n')
+				continue; // 불량 입력을 제거한다
+			    // cin.get()을 사용하여 입력 큐에 남아 있는 입력을 그 줄의 끝까지 읽어버린다
+			cout << "골프 점수를 입력하십시오: ";
+		}
+	}
+
+	// 평균을 계산한다
+	double total = 0.0;
+	for (i = 0; i < Max; i++)
+		total += golf[i];
+
+	// 결과를 보고한다
+	cout << "총 " << Max << "라운드의 평균 점수 = "
+		<< total / Max << endl;
+	return 0;
+}
