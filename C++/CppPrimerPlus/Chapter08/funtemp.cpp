0d4763c4895a715c5936a76ecdefd3b1a2803ac5
KimDoHyun <dohyun9565@gmail.com>
2021-01-18 15:49:13 +0900

    function template

---
+++ C++/CppPrimerPlus/Chapter08/funtemp.cpp
@@ -0,0 +1,34 @@
+// funtemp.cpp -- 함수 템플릿 사용하기
+#include <iostream>
+// 함수 템플릿 원형
+template <typename Any>
+void Swap(Any& a, Any& b);
+
+int main()
+{
+	using namespace std;
+	int i = 10; 
+	int j = 20;
+	cout << "i, j = " << i << ", " << j << ".\n";
+	cout << "컴파일러가 생성한 int형 교환기를 사용하면\n";
+	Swap(i, j);
+	cout << "이제 i, j = " << i << ", " << j << ".\n";
+
+	double x = 24.5;
+	double y = 81.7;
+	cout << "x, y = " << x << ", " << y << ".\n";
+	cout << "컴파일러가 생성한 double형 교환기를 사용하면\n";
+	Swap(x, y);
+	cout << "이제 x, y = " << x << ", " << y << ".\n";
+	return 0;
+}
+
+// 함수 템플릿 정의
+template<typename Any>
+void Swap(Any& a, Any& b)
+{
+	Any temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
