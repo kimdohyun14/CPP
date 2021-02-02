5a3a234ce2b4a1500c5846d911c25bb8e0c4e566
KimDoHyun <dohyun9565@gmail.com>
2021-02-02 23:49:28 +0900

    array of objects

---
+++ C++/CppPrimerPlus/Chapter10/stock20/stock20.h
@@ -0,0 +1,24 @@
+// stock20.h -- 생성자들과 소멸자가 추가된 Stock 클래스 선언
+#ifndef STOCK20_H_
+#define STOCK20_H_
+#include <string>
+
+class Stock
+{
+private:
+	std::string company;
+	long shares;
+	double share_val;
+	double total_val;
+	void set_tot() { total_val = shares * share_val; }
+public:
+	Stock(); // 디폴트 생성자
+	Stock(const std::string& co, long n = 0, double pr = 0.0);
+	~Stock(); // 소멸자
+	void buy(long num, double price);
+	void sell(long num, double price);
+	void update(double price);
+	void show() const;
+	const Stock& topval(const Stock& s) const;
+};
+#endif
