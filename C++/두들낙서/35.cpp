db3894d9c5411f853961d03e35298e4a777dbda5
paparoni95 <dohyun9565@gmail.com>
2021-06-25 19:57:02 +0900

    recursive

---
+++ C++/두들낙서/35.cpp
@@ -0,0 +1,14 @@
+// Àç±ÍÇÔ¼ö
+// recursion
+// ÆÑÅä¸®¾ó
+// 5! = 5*4*3*2*1 = 120
+// n! = n * (n-1)!
+#include <cstdio>
+int factorial(int n) {
+	if (n == 0 || n == 1) return 1;
+	return n * factorial(n - 1);
+}
+int main() {
+	printf("%d\n", factorial(5));
+	return 0;
+}
